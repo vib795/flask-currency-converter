d73fa216cd36c0793a88e9361df5ea2073ca210d
Utkarsh Singh <utkarsh.singh0609@gmail.com>
2023-12-19 13:08:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Currency Converter Flask App
+
+## Overview
+This is a simple web application built with Flask for converting currencies. It uses an external API to fetch real-time currency conversion rates.
+
+## Features
+- Currency conversion between a variety of currencies.
+- Real-time fetching of conversion rates from an external API.
+- User-friendly web interface.
+
+## Requirements
+- Python 3.10
+- Flask
+- Requests
+
+## Installation
+
+First, clone the repository to your local machine:
+
+```bash
+git clone [URL of Your Repository]
+cd currency-converter
+```
+Then, create and activate a virtual environment:
+```bash
+python -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+```
+
+Install the required packages:
+```bash
+pip install -r requirements.txt
+```
+## Usage
+To run the application, execute:
+```bash
+flask run
+```
+
+Navigate to http://127.0.0.1:5000/ in your web browser to use the application.
+
+## Contributing
+
+Contributions to this project are welcome. Please fork the repository and submit a pull request with your changes.
+
+## License
+MIT License - Feel free to use and modify this code for your personal or educational projects.
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Fetch the list of available currencies
+    response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')  # Assuming USD as a base for fetching the list
+    currencies = response.json().get('rates', {}).keys()
+
+    return render_template('index.html', currencies=currencies)
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    amount = request.form.get('amount')
+    from_currency = request.form.get('from_currency')
+    to_currency = request.form.get('to_currency')
+
+    # Fetch the list of available currencies (for dropdown)
+    response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
+    currencies = response.json().get('rates', {}).keys()
+
+    # Call the API to get the conversion rate
+    response = requests.get(f'https://api.exchangerate-api.com/v4/latest/{from_currency}')
+    rates = response.json().get('rates', {})
+
+    conversion_rate = rates.get(to_currency, 0)
+    converted_amount = float(amount) * conversion_rate if conversion_rate else 0
+
+    # Render the same page with conversion results
+    return render_template('index.html', 
+                           currencies=currencies, 
+                           converted_amount=round(converted_amount, 2), 
+                           from_currency=from_currency, 
+                           to_currency=to_currency, 
+                           amount=amount)
+
+if __name__ == '__main__':
+    app.run(debug=True)
